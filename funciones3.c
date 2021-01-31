6f50082d522d299f37d4712055e091f7ceac0175
UrtziFM <urtzi.arana13@gmail.com>
2021-01-31 21:03:03 +0100

    iterators and functions

---
+++ funciones3.c
@@ -0,0 +1,17 @@
+/*
+ Programa que muestra los números enteros comprendidos en un intervalo abierto
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void showInterval(int a, int b);
+
+int main() {
+    return 0;
+}
+
+void showInterval(int a, int b) {
+
+    
+}
